c6bfe95103970421cc2e84a47f56c87841c65a4c
my78kill <aigirlfriend143you@gmail.com>
2026-03-30 02:50:07 +0530

    Update basic.py

--- handlers/help_sections/basic.py
+++ handlers/help_sections/basic.py
@@ -1,17 +1,41 @@
+from telebot import types
+
 def show(bot, call):
 
+    markup = types.InlineKeyboardMarkup()
+    markup.add(
+        types.InlineKeyboardButton("🔙 Back", callback_data="back_help")
+    )
+
     text = """
-👨 <b>Basic Commands</b>
+<b>Base Commands</b>
 
-• /start - Start bot  
-• /help - Open help menu  
-• /settings - Open settings  
+👮🏻 Available to Admins & Moderators  
+🕵🏻 Available to Admins  
 
-More coming soon...
+👮🏻 /reload - updates the Admins list and their privileges  
+
+🕵🏻 /settings - manage all the bot settings in a group  
+
+👮🏻 /ban - ban a user permanently from the group  
+
+👮🏻 /mute - put a user in read-only mode  
+
+👮🏻 /kick - remove a user (can rejoin via link)  
+
+👮🏻 /unban - remove user from blacklist  
+
+👮🏻 /info - get user info  
+👮🏻 /infopvt - get info in private  
+
+◽️ /staff - list of group staff  
+
+✨ Gudboi
 """
 
     bot.edit_message_text(
         text,
         call.message.chat.id,
-        call.message.message_id
+        call.message.message_id,
+        reply_markup=markup
     )
