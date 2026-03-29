1dc2d861ec6f3d98d887bb9a66bd782422e84336
my78kill <aigirlfriend143you@gmail.com>
2026-03-30 02:58:46 +0530

    Update pro.py

--- handlers/help_sections/pro.py
+++ handlers/help_sections/pro.py
@@ -1,1 +1,31 @@
-h
+from telebot import types
+
+def show(bot, call):
+
+    markup = types.InlineKeyboardMarkup(row_width=1)
+
+    markup.add(
+        types.InlineKeyboardButton("⚙️ Setup Staff Group", callback_data="setup_staff"),
+        types.InlineKeyboardButton("🤖 How to Create a Clone", callback_data="clone"),
+        types.InlineKeyboardButton("👥 Users Roles", callback_data="roles"),
+    )
+
+    markup.add(
+        types.InlineKeyboardButton("🔙 Back", callback_data="back_help")
+    )
+
+    text = """
+<b>Pro Guides</b>
+
+In this menu you will find some guides for very advanced Group Help functions.
+
+⚠️ I recommend using them only if you know what you are doing  
+and follow each step carefully.
+"""
+
+    bot.edit_message_text(
+        text,
+        call.message.chat.id,
+        call.message.message_id,
+        reply_markup=markup
+    )
