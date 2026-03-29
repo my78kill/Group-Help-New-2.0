194cd837f2a479b8bda4093702a0d5d0b44e3cc8
my78kill <aigirlfriend143you@gmail.com>
2026-03-30 02:47:18 +0530

    Update basic.py

--- handlers/help_sections/basic.py
+++ handlers/help_sections/basic.py
@@ -1,1 +1,17 @@
-.
+def show(bot, call):
+
+    text = """
+👨 <b>Basic Commands</b>
+
+• /start - Start bot  
+• /help - Open help menu  
+• /settings - Open settings  
+
+More coming soon...
+"""
+
+    bot.edit_message_text(
+        text,
+        call.message.chat.id,
+        call.message.message_id
+    )
