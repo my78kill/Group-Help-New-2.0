f2bde001f0adb1461567acf555aed460f833c0d1
my78kill <aigirlfriend143you@gmail.com>
2026-03-30 03:23:26 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,11 @@
+def is_group(chat_id):
+    return str(chat_id).startswith("-100")
 
+
+def is_admin(bot, user_id, chat_id):
+    try:
+        admins = bot.get_chat_administrators(chat_id)
+        return any(admin.user.id == user_id for admin in admins)
+    except Exception as e:
+        print("Admin check error:", e)
+        return False
