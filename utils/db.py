14c7d30880136e9d19ffb3d247a9aede97d2a017
my78kill <aigirlfriend143you@gmail.com>
2026-03-30 03:22:44 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,28 @@
 
+import json
+import os
+
+DB_FILE = "groups.json"
+
+def load_data():
+    if not os.path.exists(DB_FILE):
+        return {}
+    with open(DB_FILE, "r") as f:
+        return json.load(f)
+
+def save_data(data):
+    with open(DB_FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
+def add_group(chat_id, title):
+    data = load_data()
+
+    if str(chat_id) not in data:
+        data[str(chat_id)] = {
+            "title": title
+        }
+
+    save_data(data)
+
+def get_groups():
+    return load_data()
