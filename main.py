6aaf16b2863811395ca90fbe15bbec9098ffd02c
my78kill <aigirlfriend143you@gmail.com>
2026-03-30 02:18:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from telebot import TeleBot
 from config import TOKEN
 from handlers import start, buttons, help
+from alive import keep_alive
 
 bot = TeleBot(TOKEN, parse_mode="HTML")
 
+# Start Flask server
+keep_alive()
+
+# Register handlers
 start.register(bot)
 buttons.register(bot)
 help.register(bot)
 
-print("Bot Running...")
+print("Bot + Web Alive Started...")
 
 bot.infinity_polling()
