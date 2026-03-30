7973d4223627d30a0e79316b11830347959a9b3e
my78kill <aigirlfriend143you@gmail.com>
2026-03-30 10:49:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main.py
 from telebot import TeleBot
 from config import TOKEN
 from handlers import start, buttons, help
@@ -6,7 +5,7 @@ from alive import keep_alive
 
 bot = TeleBot(TOKEN, parse_mode="HTML", threaded=True)
 
-# Start Flask server
+# Flask server
 keep_alive()
 
 # Register handlers
@@ -16,5 +15,4 @@ help.register(bot)
 
 print("Bot + Web Alive Started...")
 
-# Use allowed_updates here instead
-bot.infinity_polling(allowed_updates=['message', 'callback_query', 'chat_member'])
+bot.infinity_polling()
